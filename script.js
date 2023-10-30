ab6018261a4bc08d82a809ae16173a02e746b16f
LuannLopes <luanlopesofc@gmail.com>
2023-10-30 18:51:20 -0300

    application update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(menuOptions);
+const list = document.querySelector("ul");
+const showAllButton = document.querySelector(".show-all");
+let myLi = "";
+
+function showAll() {
+  menuOptions.forEach((product) => {
+    myLi += `
+    <li>
+      <img src="${product.src}" alt="${product.name}">
+      <p>${product.name}</p>
+      <p class="item-price">R$ ${product.price}</p>
+    </li>`;
+  });
+
+  list.innerHTML = myLi;
+}
+
+showAllButton.addEventListener("click", showAll);
